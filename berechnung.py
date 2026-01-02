6044ebd90fd774cfd000ac36440b9fa914892873
MHofmann0 <max.hofmann@study.thws.de>
2026-01-02 15:26:27 +0100

    CSB Berechnung fertig

--- berechnung.py
+++ berechnung.py
@@ -11,7 +11,7 @@ fs = 0.05 #inerte Fraktion im Zulauf
 fa = 0.30 #inerte Fraktion im partikulären CSB
 n = 0.5 #hydraulischer Koeffizient
 k_20 = 0.0024 #Reaktionskonstante
-höhe_TK = 5.2 #höhe Tropfkörper
+hoehe_TK = 5.2 #höhe Tropfkörper
 A_spez = 125 #spezifische Oberfläche des Tropfkörpers in [m²/m³]
 O_C_20 = 1.03 #Temperaturkoeffizient
 h_seg = 0.1 #Segmenthöhe in [m]
@@ -33,11 +33,26 @@ X_CSB_abb_ZT = X_CSB_ZT - X_CSB_inert_ZT
 S_CSB_abb_ZT = S_CSB_abb_ZT + 0.5 * X_CSB_abb_ZT
 
 #Anpassung des k-20 Wertes
-k_20_angepasst = k_20 * (5.2 / höhe_TK) ** n 
+k_20_angepasst = k_20 * (5.2 / hoehe_TK) ** n 
 
-#modifizierte Velz-Gleichung
-exponent =  (A_spez * k_20_angepasst * O_C_20 ** (T-20) + h_seg) /(q_A ** n)
-S_SCB_abb_AT = S_CSB_abb_ZT * math.exp(-exponent)
+
+zaehler_S_CSB_hoehe = 0
+ergebnis_liste = []
+ergebnis_liste = [[zaehler_S_CSB_hoehe], [S_CSB_abb_ZT]]
+
+while(zaehler_S_CSB_hoehe < hoehe_TK):
+    #modifizierte Velz-Gleichung
+    exponent =  (A_spez * k_20_angepasst * O_C_20 ** (T-20) + h_seg) /(q_A ** n)
+    S_SCB_abb_AT = S_CSB_abb_ZT * math.exp(-exponent)
+    
+    zaehler_S_CSB_hoehe = zaehler_S_CSB_hoehe + h_seg
+    
+    ergebnis_liste[0].append(zaehler_S_CSB_hoehe)
+    ergebnis_liste[1].append(S_SCB_abb_AT)
+
+    S_CSB_abb_ZT = S_SCB_abb_AT
+
+return ergebnis_liste
 
 
 
